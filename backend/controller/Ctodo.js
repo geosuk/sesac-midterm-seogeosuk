1d07158df14ac46ebe647207f2efc16b2dec82ff
geosuk <geosuk9808@gmail.com>
2024-07-31 18:18:33 +0900

    최종수정

--- backend/controller/Ctodo.js
+++ backend/controller/Ctodo.js
@@ -4,7 +4,7 @@ exports.index = (req, res)=>{
 }
 
 //타이틀 및 done 가져오기
-exports.getTitle = async(req, res)=>{
+exports.getTodo = async(req, res)=>{
     try{
         const {id} = req.params;
         const Todo = await Todo.findOne({
@@ -16,8 +16,46 @@ exports.getTitle = async(req, res)=>{
                 }
             ]
         });
-        res.json(todo);
+        if (!Todo){
+            return res.sendStatus(404);
+        }
+        res.json(getTodo);
     }catch(err){
         res.status(500).send("Internal Server Error");
+    }
+}
+// Todo 수정
+exports.patchTodo = async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const updateTodo = await Todo.update(
+            {title},
+            {where: {id}}
+        )
+        if(!updateTodo){
+            return res.sendStatus(404);
+        }
+        req.json(updateTodo);
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+//Todo 삭제
+exports.deleteTodo = async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const isDeleted = await Todo.destroy({
+            where: {id}
+        });
+        if(isDeleted){
+            return res.send(true);
+        }else{
+            return res.send(false);
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
 }
--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -2,6 +2,9 @@ const express =require('express');
 const router = express.Router();
 const controller = require('../controller/Ctodo');
 
-router.get('/', controller.getTitle);
-
+router.get('/todos', controller.getTodo);
+router.get('/todos/:id', controller.getTodo);
+router.patch('/todos')
+router.patch('/todos/:id', controller.patchTodo);
+router.delete('/todos/:id', controller.deleteTodo)
 module.exports = router;
